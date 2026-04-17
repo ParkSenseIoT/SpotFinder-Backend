802cba2d0a6a24c92a474a0e023d92fa127726ab
Elandrehs <andrestheb@gmail.com>
2026-04-17 14:03:44 -0500

    feat(parkingmonitoring): add ParkingSlot repository

---
+++ src/main/java/com/spotfinderbackend/parkingmonitoring/infrastructure/persistence/jpa/repositories/ParkingSlotRepository.java
@@ -0,0 +1,19 @@
+package com.spotfinderbackend.parkingmonitoring.infrastructure.persistence.jpa.repositories;
+
+import com.spotfinderbackend.parkingmonitoring.domain.model.aggregates.ParkingSlot;
+import com.spotfinderbackend.parkingmonitoring.domain.model.valueobjects.ParkingSlotStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ParkingSlotRepository extends JpaRepository<ParkingSlot, Long> {
+
+    // TS32 → disponibles
+    List<ParkingSlot> findByStatus(ParkingSlotStatus status);
+
+    // para validaciones (ej: evitar duplicados de código)
+    Optional<ParkingSlot> findByCode(String code);
+
+    boolean existsByCode(String code);
+}
