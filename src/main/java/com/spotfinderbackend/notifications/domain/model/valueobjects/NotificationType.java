88e1aff646cd977be5f92e44a9dc836c1cccf582
Elandrehs <andrestheb@gmail.com>
2026-04-17 15:10:07 -0500

    feat(notifications): add NotificationStatus and NotificationType value objects

---
+++ src/main/java/com/spotfinderbackend/notifications/domain/model/valueobjects/NotificationType.java
@@ -0,0 +1,9 @@
+package com.spotfinderbackend.notifications.domain.model.valueobjects;
+
+public enum NotificationType {
+    ENTRY_CONFIRMED,
+    PAYMENT_SUCCESS,
+    PAYMENT_PENDING,
+    EMERGENCY_ALERT,
+    SYSTEM_INFO
+}
