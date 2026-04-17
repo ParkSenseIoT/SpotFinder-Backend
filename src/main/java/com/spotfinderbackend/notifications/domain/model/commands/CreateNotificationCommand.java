ca77826de39f765f1dd9d768223c94acc51c54c9
Elandrehs <andrestheb@gmail.com>
2026-04-17 15:24:38 -0500

    feat(notifications): implement NotificationCommandService and enforce NotificationType enum in command

--- src/main/java/com/spotfinderbackend/notifications/domain/model/commands/CreateNotificationCommand.java
+++ src/main/java/com/spotfinderbackend/notifications/domain/model/commands/CreateNotificationCommand.java
@@ -1,17 +1,22 @@
 package com.spotfinderbackend.notifications.domain.model.commands;
 
+import com.spotfinderbackend.notifications.domain.model.valueobjects.NotificationType;
 import com.spotfinderbackend.shared.domain.model.exceptions.BadRequestException;
 
 public record CreateNotificationCommand(
         Long userId,
-        String type,
+        String title,
+        NotificationType type,
         String message
 ) {
     public CreateNotificationCommand {
         if (userId == null || userId <= 0)
             throw new BadRequestException("UserId is required");
 
-        if (type == null || type.isBlank())
+        if (title == null || title.isBlank())
+            throw new BadRequestException("Title is required");
+
+        if (type == null)
             throw new BadRequestException("Notification type is required");
 
         if (message == null || message.isBlank())
