eaed0869b3f9427626690548c77abe6e60a116d6
Elandrehs <andrestheb@gmail.com>
2026-04-19 00:04:15 -0500

    feat(accesscontrol): implement initial access control command handling logic

--- src/main/java/com/spotfinderbackend/accesscontrol/application/internal/commandservices/AccessControlCommandServiceImpl.java
+++ src/main/java/com/spotfinderbackend/accesscontrol/application/internal/commandservices/AccessControlCommandServiceImpl.java
@@ -1,4 +1,51 @@
 package com.spotfinderbackend.accesscontrol.application.internal.commandservices;
 
-public class AccessControlCommandServiceImpl {
+import com.spotfinderbackend.accesscontrol.domain.model.commands.*;
+import com.spotfinderbackend.accesscontrol.domain.model.valueobjects.AlprResult;
+import com.spotfinderbackend.accesscontrol.domain.services.AccessControlCommandService;
+import com.spotfinderbackend.notifications.domain.model.commands.CreateNotificationCommand;
+import com.spotfinderbackend.notifications.domain.services.NotificationCommandService;
+import com.spotfinderbackend.parkingsessions.domain.model.commands.CreateParkingSessionCommand;
+import com.spotfinderbackend.parkingsessions.domain.model.commands.EndParkingSessionCommand;
+import com.spotfinderbackend.parkingsessions.domain.model.queries.GetActiveSessionQuery;
+import com.spotfinderbackend.parkingsessions.domain.services.ParkingSessionCommandService;
+import com.spotfinderbackend.parkingsessions.domain.services.ParkingSessionQueryService;
+import com.spotfinderbackend.shared.domain.model.exceptions.BusinessRuleException;
+import com.spotfinderbackend.shared.domain.model.exceptions.NotFoundException;
+import com.spotfinderbackend.vehicles.domain.model.aggregates.Vehicle;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccessControlCommandServiceImpl implements AccessControlCommandService {
+
+    private final ParkingSessionCommandService sessionCommandService;
+    private final ParkingSessionQueryService sessionQueryService;
+    private final NotificationCommandService notificationCommandService;
+
+    public AccessControlCommandServiceImpl(
+
+            ParkingSessionCommandService sessionCommandService,
+            ParkingSessionQueryService sessionQueryService,
+            NotificationCommandService notificationCommandService
+    ) {
+        this.sessionCommandService = sessionCommandService;
+        this.sessionQueryService = sessionQueryService;
+        this.notificationCommandService = notificationCommandService;
+    }
+
+
+    @Override
+    public void handle(RegisterEntryCommand command) {
+
+    }
+
+    @Override
+    public void handle(RegisterExitCommand command) {
+
+    }
+
+    @Override
+    public AlprResult handle(ProcessAlprCommand command) {
+        return null;
+    }
 }
